db3f383719541968de7c98989bc0f6b9eb018152
Daniel Patterson <dbp@dbpmail.net>
2017-07-13 16:16:35 -0400

    Attempt #1 for travis :)

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
-REF=`git rev-parse HEAD`
-BRANCH=`git rev-parse --abbrev-ref HEAD`
+#!/bin/bash
+set -e
 
-git branch -D autodeploy
-git checkout -b autodeploy
+tee ~/.netrc > /dev/null <<EOF
+machine github.com
+login silc-bot
+password $GH_TOKEN
+EOF
 
-./site.hs build
+c6d20c318057cc611022da4c5c2280a467938956
+
 rsync -a --filter='P _site/'      \
       --filter='P _cache/'     \
       --filter='P .git/'       \
@@ -13,11 +17,26 @@ rsync -a --filter='P _site/'      \
       --delete-excluded        \
       _site/ .
 
+
+git remote add deploy https://github.com/silcgroup/silcgroup.github.io.git
+git config --global user.email "dbp+silc@dbpmail.net"
+git config --global user.name "SILC Bot"
+git fetch deploy
+
+REV="** deploy silcgroup/website@$TRAVIS_COMMIT"
+git fetch deploy
 git reset --soft deploy/master
-git add -A
-git commit -m "Deploy silcgroup/website@$REF"
+git add .
+git status
+git commit -m "$REV"
 
-git push deploy autodeploy:master
-
-git checkout $BRANCH
-git branch -D autodeploy
+git fetch origin
+CURRENT_HEAD=`git rev-parse origin/master`
+if [ "$TRAVIS_COMMIT" = "$CURRENT_HEAD" ]
+then
+    echo "Committing..."
+    git push deploy HEAD:refs/heads/master
+else
+    echo "Aborting; detected race..."
+    exit 1
+fi
